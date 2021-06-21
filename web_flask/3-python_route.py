85e555faf2b6041d053d8f30927ea9ff322b609a
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-06-21 15:43:40 +0000

    add 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" this program starts a flask web application """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def howdy():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def bonjour(text):
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route('/python/(<text>)')
+def hola(text="is cool"):
+    return 'Python {}'.format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
